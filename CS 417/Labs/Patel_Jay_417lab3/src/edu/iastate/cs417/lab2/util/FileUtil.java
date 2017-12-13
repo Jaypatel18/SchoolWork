28944f5601e024f55810529a6b404ae3e7dbe329
Jay Patel <jaypatel@mac-air-28csiastateedu.local>
2017-12-12 21:22:18 -0600

    new files

--- CS 417/Labs/Patel_Jay_417lab3/src/edu/iastate/cs417/lab2/util/FileUtil.java
+++ CS 417/Labs/Patel_Jay_417lab3/src/edu/iastate/cs417/lab2/util/FileUtil.java
@@ -6,7 +6,33 @@ import java.io.FileReader;
 import java.util.Vector;
 
 public class FileUtil {
-	// read test data from a file including input data and expected results
+
+	/**
+	 * Reads test set data from a tab-delimited text file. 
+	 * 
+	 * Ignores the first line. The first line is assumed to contain column headers to 
+	 * facilitate easy maintenance of the test set. 
+	 * 
+	 * Each line after the header is assumed to contain a complete set of values for 
+	 * one test case. Each line will be returned as an array of strings, regardless of 
+	 * whether some values are numeric or not. 
+	 * 
+	 * @param filename The file containing the data. Assumed to be relative to the working directory. 
+	 * In eclipse, this is normally the project directory (top-level) 
+	 * 
+	 * @param cols the number of columns in the data. 
+	 * @return The return object is an array of arrays of String. I.e., 
+	 * if rval is a reference to the returned object, then rval[1] is the array
+	 * containing the first data set. 
+	 * 
+	 * throws IllegalArgumentException if filename is not found, or is not readable. 
+	 * throws InvalidDataException if file is empty, or has only one line (i.e., the file should contain 
+	 * more than just a header. 
+	 * throws MalformedTestSet, reporting the offending line number in the text file,
+	 * if any line does not have 'cols' data items. 
+	 * 
+	 */
+	
 	public static Object[][] getParametersFromFile(String filename, int cols) {
 		try {
 			File f = new File(filename);
@@ -42,6 +68,8 @@ public class FileUtil {
 		} catch (Exception e){
 			return null; 
 		}
+		
+		
 	}
 
 }
